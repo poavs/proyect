c9e909e20e3836ed4aa02d0d41eb2c2d6aa47992
JairMolina10 <jairmolina368@gmail.com>
2024-11-21 23:45:06 -0600

    practica de programacion
    Subir sus 4 ejercicios de clase del 23/09/2024

--- app.js
+++ app.js
@@ -1,10 +1,40 @@
-let numero1 = parseFloat(prompt("Ingresa el primer número:"));
-let numero2 = parseFloat(prompt("Ingresa el segundo número:"));
-
-// Calcula la suma
-function suma(numero1 , numero2){
-    return numero1+numero2;
+/*function imprimirEnVertical() {
+    let palabra = "xxxx";
+    for (let i = 0; i < palabra.length; i++) {
+        console.log(palabra[i]);
+    }
 }
 
-// Muestra el resultado en la consola
-console.log("La suma de "+ numero1 + " y "+ numero2 + " es igual a:  " +suma(numero1,numero2));
+imprimirEnVertical();
+*/
+
+/*
+function imprimirEnHorizontal() {
+    let palabra = "xxxx";
+    console.log(palabra);
+}
+
+imprimirEnHorizontal();
+*/
+
+/*
+function imprimirTablasDeMultiplicar() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`Tabla del ${i}:`);
+        for (let j = 1; j <= 10; j++) {
+            console.log(`${i} x ${j} = ${i * j}`);
+        }
+        console.log(''); // Salto de línea entre tablas
+    }
+}
+
+imprimirTablasDeMultiplicar();
+*/
+
+const imprimirFrutasConIndice = () => {
+    let frutas = ["Manzana", "Banana", "Naranja", "Pera", "Uva", "Mango", "Piña", "Melón", "Sandía", "Durazno"];
+    frutas.forEach((fruta, indice) => console.log(`${indice}: ${fruta}`));
+};
+
+imprimirFrutasConIndice();
+
